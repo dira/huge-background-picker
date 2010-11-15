f24e0c58de75d7caf8ccdd844edc6e0e0427bcbe
Irina Dumitrascu <me@dira.ro>
2010-11-15 18:37:20 -0400

    Moved initialization to JS

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,28 +1,33 @@
 $(document).ready(init);
 
 function init() {
-  color_field = $('.color');
-  color_field.change(refresh);
-  color_field.change(update_location);
+  color_field = $('input');
 
+  init_color_field();
   init_history();
-  color_field.change(to_history);
 
-  window.setTimeout(init_color, 0);
+  color_field.change(update_bgrd);
+  color_field.change(update_location);
+  color_field.change(to_history);
 }
 
-function init_color() {
-  set_color(document.location.hash == "" ? '99aa44' : document.location.hash);
-  color_field[0].color.showPicker();
+function init_color_field() {
+  var bgrd_color = RGBToHex($('#wrapper').css('background-color'));
+  picker = new jscolor.color(color_field[0], {
+    pickerFaceColor: bgrd_color,
+    pickerBorderColor: bgrd_color,
+    pickerOnfocus: false,
+    caps: false
+  })
+  picker.showPicker();
+  window.setTimeout(function() {
+    set_color(document.location.hash == "" ? '99aa44' : document.location.hash)
+  }, 0)
 }
 
 function set_color(color) {
-  color_field[0].color.fromString(color);
+  picker.fromString(color);
   color_field.change();
-}
-
-function refresh() {
-  set_bgrd(get_color());
 }
 
 function update_location() {
@@ -30,17 +35,27 @@ function update_location() {
 }
 
 function get_color() {
-  return '' + color_field.attr('color')
+  return "" + picker
 }
 
-function set_bgrd(color) {
-  $('body').css('background-color', '#' + color);
+function update_bgrd(color) {
+  $('body').css('background-color', '#' + get_color());
 }
 
 
 function init_history() {
+  color_history = [];
   $('#wrapper').append('<section><h1>History</h1><div id="history"/></section>');
-  color_history = []
+  $('section').delay(2000).fadeOut(1000);
+  $('#wrapper').mouseover(show_history)
+  $('#wrapper').mouseout(hide_history)
+}
+
+function show_history() {
+  $('section').show();
+}
+function hide_history() {
+  $('section').hide();
 }
 
 function to_history() {
@@ -59,4 +74,17 @@ function to_history() {
   container.click(function() {
     set_color(container.data('color'));
   })
+}
+
+function RGBToHex(rgb) {
+  function to_padded_hex(nr) {
+    var result = new Number(nr).toString(16);
+    return result.length == 1 ? "0" + result : result;
+  }
+  var rgb = rgb.replace(/rgb|\(|\)| /g, "").split(',')
+  var hex = "#";
+  for (var i = 0; i < rgb.length; i++) {
+    hex += to_padded_hex(rgb[i]);
+  }
+  return hex;
 }
